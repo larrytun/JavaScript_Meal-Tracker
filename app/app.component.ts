78c2879c9238996ba6b2db2141e2586188140de0
larrytun <larrydeantun@gmail.com>
2016-10-21 16:29:19 -0700

    Add another entry and finish project

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,19 +6,19 @@ import { Entry } from './entry.model';
   template: `
   <div class="container body">
     <div class="jumbotron">
-      <h1>Food Blog</h1>
+      <h1>My Daily Food Blog</h1>
     </div>
-    <new-entry
-      (newEntrySender)="addEntry($event)"
-    ></new-entry>
-    <edit-entry
-      [childSelectedEntry]="selectedEntry"
-      (doneClickedSender)="finishedEditing()"
-    ></edit-entry>
     <entry-list
       [childEntryList]="masterEntryList"
       (clickSender)="showDetails($event)"
       ></entry-list>
+    <new-entry
+      (newEntrySender)="addEntry($event)"
+    ></new-entry>
+    <edit-entry id="editEntry"
+      [childSelectedEntry]="selectedEntry"
+      (doneClickedSender)="finishedEditing()"
+    ></edit-entry>
   </div>
 
   `
@@ -26,9 +26,10 @@ import { Entry } from './entry.model';
 
 export class AppComponent {
   public masterEntryList: Entry[] = [
-    new Entry("Pizza", 350, "I had one big slice of pizza"),
-    new Entry("Hot Dogs", 400, "I ate 3 kosher hot dogs"),
-    new Entry("Chicken Salad", 320, "I ate half a chicken salad"),
+    new Entry("07/01/2016","Carrots", 50, "I had a few baby carrots with ranch"),
+    new Entry("07/04/2016","Pizza", 350, "I had one big slice of pizza"),
+    new Entry("07/15/2016","Hot Dogs", 400, "I ate 3 kosher hot dogs"),
+    new Entry("07/27/2016","Chicken Salad", 320, "I ate half a chicken salad"),
   ];
   selectedEntry: Entry = null;
   showDetails(clickedEntry: Entry) {
